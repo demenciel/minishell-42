50f0e55bc29fc1b0cd53f4dd35e57ca28d3fa10a
Alexandre Couture <acouture@c1r6p4.42quebec.com>
2023-05-29 14:40:11 -0400

    a

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: acouture <acouture@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 16:57:16 by acouture          #+#    #+#             */
-/*   Updated: 2023/05/29 14:37:55 by acouture         ###   ########.fr       */
+/*   Updated: 2023/05/29 14:39:45 by acouture         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-// PARSING
+// EXEC
 int main( int ac, char **av)
 {
     (void)ac;
